278147e50c5ad7a6dacfbffbbe91f64038ba7933
Richard Baca <rb1224@ic.ac.uk>
2025-05-29 15:36:31 +0100

    Add comments to DecodedInstruction

--- src/instruction_types.h
+++ src/instruction_types.h
@@ -19,37 +19,37 @@ typedef struct {
 		 
 		 union {
         struct {  // DP Immediate
-            uint8_t sf;
-            uint8_t opc;
-            uint8_t opi;
-            uint32_t operand;
-            uint8_t rd;
+            uint8_t sf; // Register size flag (0: 32b, 1: 64b)
+            uint8_t opc; // Opcode
+            uint8_t opi; // Operation interpretation
+            uint32_t operand; // Structure depends on opi
+            uint8_t rd; // Destination register
         } dp_imm;
 
         struct {  // DP Register
-            uint8_t sf;
-            uint8_t opc;
-            uint8_t M;
-            uint8_t opr;
-            uint8_t rm;
-            uint8_t operand;
-            uint8_t rn;
-            uint8_t rd;
+            uint8_t sf; // Register size flag (0: 32b, 1: 64b)
+            uint8_t opc; // Opcode
+            uint8_t M; // Op interpretation
+            uint8_t opr; // Op interpretation
+            uint8_t rm; // Second operand register
+            uint8_t operand; // Structure depends on M and opr
+            uint8_t rn; // First operand register
+            uint8_t rd; // Destination register
         } dp_reg;
 
         struct {  // Single Data Transfer
-            uint8_t sf;
-						uint8_t U;
-            uint8_t L;
-            uint16_t offset;
-            uint8_t xn;
-            uint8_t rt;
+            uint8_t sf; // Register size flag (0: 32b, 1: 64b)
+						uint8_t U; // Unsigned offset flag (0: signed, 1: unsigned)
+            uint8_t L; // Load flag (0: store, 1: load)
+            uint16_t offset; // Structure depends on U and bit 21
+            uint8_t xn; // Base register
+            uint8_t rt; // Target register
         } sdt;
 
         struct {  // Load Literal
-						uint8_t sf;
-            int32_t simm19; 
-            uint8_t rt;
+						uint8_t sf; // Register size flag (0: 32b, 1: 64b)
+            int32_t simm19; // Literal offset
+            uint8_t rt; // Target register
         } load_lit;
 
         struct {  // Branch
