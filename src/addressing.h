e148af4913cb1e556d6632cc0fefe63e4d10f8af
Mirza Baig <zmb24@willow17.doc.ic.ac.uk>
2025-05-29 13:52:42 +0100

    FIX: Added sign flag parameter to addressing functions (addressing.h) [zmb24]

--- src/addressing.h
+++ src/addressing.h
@@ -15,14 +15,14 @@ typedef enum {
 // Each function here takes the number of the register (X0-30) rather than its contents
 // Each argument has been bit extended to 64 bits
 // Unsigned Immediate (U = 1)
-uint64_t get_address_unsigned_immediate(ARMState* state, uint64_t imm12, uint8_t register_xn);
+uint64_t get_address_unsigned_immediate(ARMState* state, uint64_t imm12, uint8_t register_xn, uint8_t sf);
 // Pre-Indexed (I = 1)
-uint64_t get_address_pre_indexed(ARMState* state, int64_t simm9, uint8_t register_xn);
+uint64_t get_address_pre_indexed(ARMState* state, int64_t simm9, uint8_t register_xn, uint8_t sf);
 // Post-Indexed (I = 0)
-uint64_t get_address_post_indexed(ARMState* state, int64_t simm9, uint8_t register_xn);
+uint64_t get_address_post_indexed(ARMState* state, int64_t simm9, uint8_t register_xn, uint8_t sf);
 // Register Offset 
-uint64_t get_address_register_offset(ARMState* state, uint8_t register_xm, uint8_t register_xn);
+uint64_t get_address_register_offset(ARMState* state, uint8_t register_xm, uint8_t register_xn, uint8_t sf);
 // Load Literal
-uint64_t get_address_load_literal(ARMState* state, int32_t simm19);
+uint64_t get_address_load_literal(ARMState* state, int32_t simm19, uint8_t sf);
 
 #endif
