52a15de9ee9317046fd4e645624dcf6e815a3b1b
Mirza Baig <zmb24@willow17.doc.ic.ac.uk>
2025-05-29 14:26:17 +0100

    Implement unconditional branch (mem_branch_executor.c) [zmb24]

--- src/mem_branch_executor.c
+++ src/mem_branch_executor.c
@@ -1,2 +1,14 @@
 #include "mem_branch_executor.h"
 // addressing.h included implicitly through mem_branch_executor.h
+
+// PC = PC + 4 * simm26
+void execute_branch_unconditional(ARMState* state, int64_t simm26) {
+    uint64_t next_pc;
+    uint64_t curr_pc;
+    int64_t offset; // Signed
+
+    offset = simm26 << 2;
+    curr_pc = state->pc;
+    next_pc = curr_pc + offset;
+    state->pc = next_pc;
+}
