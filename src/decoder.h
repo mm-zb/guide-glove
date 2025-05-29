e18a8ea8c970ed563998544e2c526cf3e40a46f1
Richard Baca <rb1224@ic.ac.uk>
2025-05-29 20:59:20 +0100

    Add missing protos to decoder.h [rb1224]

--- src/decoder.h
+++ src/decoder.h
@@ -6,5 +6,7 @@
 #include "instruction_types.h"
 
 DecodedInstruction decode_instruction(uint32_t instruction_word);
+uint32_t get_bits(uint32_t value, uint8_t start, uint8_t end);
+InstructionType get_instruction_type(uint32_t instruction_word);
 
 #endif
