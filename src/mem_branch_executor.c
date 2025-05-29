79a8e694eb665f3efa8750ff0adbce6d5dc2e2f8
Mirza Baig <zmb24@willow17.doc.ic.ac.uk>
2025-05-29 14:34:40 +0100

    Implement register branch (mem_branch_executor.c) [zmb24]

--- src/mem_branch_executor.c
+++ src/mem_branch_executor.c
@@ -12,3 +12,21 @@ void execute_branch_unconditional(ARMState* state, int64_t simm26) {
     next_pc = curr_pc + offset;
     state->pc = next_pc;
 }
+
+// PC = Xn
+void execute_branch_register(ARMState* state, uint8_t register_xn) {
+    uint8_t xzr = 0x1F;
+    uint64_t next_pc;
+    uint64_t register_data;
+
+    if (register_xn == xzr) {
+        // We are at the zero register, we do not need to handle this case
+        // So abort early and the PC remains unchanged
+        // This might need to set the PC to zero but I am unsure for now
+        return;
+    }
+
+    register_data = state->registers[register_xn];
+    next_pc = register_data;
+    state->pc = next_pc;
+}
