300d9493b12e295d3cf6eb40e993e5ade3b59679
Mirza Baig <zmb24@willow17.doc.ic.ac.uk>
2025-05-29 20:50:02 +0100

    Partially implement store execute instruction (mem_branch_executor.c) [zmb24]

--- src/mem_branch_executor.c
+++ src/mem_branch_executor.c
@@ -1,5 +1,7 @@
 #include "mem_branch_executor.h"
 // addressing.h included implicitly through mem_branch_executor.h
+#include "decoder.h"
+// Required for get_bits function
 
 // PC = PC + 4 * simm26
 void execute_branch_unconditional(ARMState* state, int64_t simm26) {
@@ -73,4 +75,64 @@ void execute_branch_cond(ARMState* state, int64_t simm19, uint8_t cond) {
     }
 
     execute_branch_unconditional(state, simm19);
+}
+
+// Calculates address and moves data into memory, from register rt
+void execute_str(ARMState* state, addressing_mode addr_mode, DecodedInstruction* instruction) {
+    uint64_t address;
+    uint16_t offset;
+    uint8_t sf;
+
+    uint8_t register_xn;
+    uint8_t register_xm;
+    uint8_t register_rt;
+    uint64_t register_data;
+
+    int64_t simm9;
+    int32_t simm19;
+
+    offset = instruction->offset;
+    sf = instruction->sf;
+    register_xn = instruction->xn;
+    register_rt = instruction->rt;
+    register_data = state->registers[register_rt];
+
+    // Common values to all addressing modes
+
+    switch (addr_mode) {
+        case (UNSIGNED_IMMEDIATE): 
+            address = get_address_unsigned_immediate(state, (uint64_t)offset, register_xn, sf);
+            break;
+        case (PRE_INDEXED):
+            simm9 = (int64_t)get_bits((uint32_t)offset, 12, 20);
+            // Takes the correct 9 bits for the offset
+
+            address = get_address_pre_indexed(state, simm9, register_xn, sf);
+            break;
+        case (POST_INDEXED):
+            simm9 = (int64_t)get_bits((uint32_t)offset, 12, 20);
+            // Repeated line cannot be avoided
+
+            address = get_address_post_indexed(state, simm9, register_xn, sf);
+            break;
+        case (REGISTER_OFFSET):
+            register_xm = (uint8_t)get_bits((uint32_t)offset, 16, 20);
+            address = get_address_register_offset(state, register_xm, register_xn, sf);
+            break;
+        default:
+            // Invalid addressing mode
+            // Do nothing for now
+            return;
+    }
+
+    // TODO: Add GPIO detection
+    // TODO: Add conditional write depending on sf
+    if (sf == 0) { // Store a 32-bit word
+
+    } else { // Store a 64-bit word
+
+    }
+
+    state->memory[address] = register_data;
+    // Temporary to have some effect
 }
