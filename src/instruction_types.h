2ed42b7d3c4f9c8151742db4bf9a00f8897f42e3
Richard Baca <rb1224@ic.ac.uk>
2025-05-29 16:34:43 +0100

    Flatten DecodedInstruction struct for simpler member access [rb1224]

--- src/instruction_types.h
+++ src/instruction_types.h
@@ -4,58 +4,41 @@
 #include <stdint.h>
 
 typedef enum {
-		DP_IMM,			 // Data Processing with immediate value
-		DP_REG,      // Data Processing with register
-		SDT,         // Single Data Transfer
-		LL,					 // Load Literal
-		BRANCH,     
-		HALT,        
-		UNKNOWN      // Unknown or unrecognized
+    DP_IMM,  // Data Processing with immediate value
+    DP_REG,  // Data Processing with register
+    SDT,     // Single Data Transfer
+    LL,      // Load Literal
+    BRANCH,
+    HALT,
+    UNKNOWN  // Unknown or unrecognized
 } InstructionType;
 
 typedef struct {
-     uint32_t raw_instruction;
-     InstructionType type;
-		 
-		 union {
-        struct {  // DP Immediate
-            uint8_t sf; // Register size flag (0: 32b, 1: 64b)
-            uint8_t opc; // Opcode
-            uint8_t opi; // Operation interpretation
-            uint32_t operand; // Structure depends on opi
-            uint8_t rd; // Destination register
-        } dp_imm;
+    uint32_t raw_instruction;
+    InstructionType type;
 
-        struct {  // DP Register
-            uint8_t sf; // Register size flag (0: 32b, 1: 64b)
-            uint8_t opc; // Opcode
-            uint8_t M; // Op interpretation
-            uint8_t opr; // Op interpretation
-            uint8_t rm; // Second operand register
-            uint8_t operand; // Structure depends on M and opr
-            uint8_t rn; // First operand register
-            uint8_t rd; // Destination register
-        } dp_reg;
+    uint8_t sf;        // Register size flag (0: 32b, 1: 64b)
 
-        struct {  // Single Data Transfer
-            uint8_t sf; // Register size flag (0: 32b, 1: 64b)
-						uint8_t U; // Unsigned offset flag (0: signed, 1: unsigned)
-            uint8_t L; // Load flag (0: store, 1: load)
-            uint16_t offset; // Structure depends on U and bit 21
-            uint8_t xn; // Base register
-            uint8_t rt; // Target register
-        } sdt;
+    // DP_IMM and DP_REG specific fields
+    uint8_t opc;       // Opcode
+    uint32_t operand;  // Structure depends on opi
+    uint8_t M;         // Op interpretation
+    
+    uint8_t opi;       // Operation interpretation
+    uint8_t opr;       // Op interpretation
+    uint8_t rm;        // Second operand register
+    uint8_t rn;        // First operand register
 
-        struct {  // Load Literal
-						uint8_t sf; // Register size flag (0: 32b, 1: 64b)
-            int32_t simm19; // Literal offset
-            uint8_t rt; // Target register
-        } load_lit;
+    uint8_t rd;        // Destination register
 
-        struct {  // Branch
-            int32_t operand;
-        } branch;
-    };
- } DecodedInstruction;
+    // SDT and LL specific fields
+    uint8_t U;         // Unsigned offset flag (0: signed, 1: unsigned)
+    uint8_t L;         // Load flag (0: store, 1: load)
+    uint16_t offset;   // Structure depends on U and bit 21
+    uint8_t xn;        // Base register
+    uint8_t rt;        // Target register
+    int32_t simm19;    // Literal offset
+
+} DecodedInstruction;
 
 #endif
