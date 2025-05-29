7ab8608a4ec0373ac2ad7fc860b919a29adf0ae3
Richard Baca <rb1224@ic.ac.uk>
2025-05-29 17:08:22 +0100

    Fix mismatched function proto type in header [rb1224]

--- src/decoder.h
+++ src/decoder.h
@@ -2,11 +2,8 @@
 #define DECODER_H
 
 #include <stdint.h>
-#include "arm_state.h" // For PSTATE flags, if needed by helper functions
-#include "instruction_types.h" // If DecodedInstruction is defined
+#include "instruction_types.h"
 
-// For now it can return a simple representation or just be void/uint32_t
-// TODO: Richard will refine this return type to a comprehensive struct
-uint32_t decode_instruction(uint32_t instruction_word);
+DecodedInstruction decode_instruction(uint32_t instruction_word);
 
 #endif
