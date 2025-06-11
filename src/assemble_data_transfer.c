07d123333825d7a499d04976c868454e690c8686
Mirza Baig <zmb24@willow17.doc.ic.ac.uk>
2025-06-11 14:07:16 +0100

    Implement assemble_ldr function [zmb24]

--- src/assemble_data_transfer.c
+++ src/assemble_data_transfer.c
@@ -1,1 +1,91 @@
+#include <stdbool.h>
+
 #include "assemble_data_transfer.h"
+
+// Defines shift amounts for each section of the instruction
+#define XN_START_BIT 5
+#define XM_START_BIT 16
+#define IMM12_START_BIT 10
+#define SIMM9_START_BIT 12
+#define SIMM19_START_BIT 5
+
+// Helper to check the addressing mode class
+static bool is_sdt(AddressingMode mode) {
+    return (mode != LOAD_LITERAL);
+    // If invalid addressing mode this falls into undefined behaviour
+    // Pre-condition of valid addressing mode
+}
+
+uint32_t assemble_ldr(char** tokens, SymbolTable* symbol_table, uint32_t current_address) {
+    uint32_t instruction = 0;
+    ParsedAddress address;
+    
+    AddressingMode mode;
+    uint32_t rt;
+    uint32_t xn;
+    uint32_t xm;
+    uint32_t sf;
+    uint32_t imm12; 
+    int32_t simm9; 
+    int32_t simm19;
+
+    // Initialising variables and shifting to correct locations
+    address = parse_address(tokens, symbol_table, current_address);
+    mode = address.mode;
+    rt = address.sdt_ll_reg_rt; // No shift
+    xn = address.sdt_reg_xn << XN_START_BIT;
+    xm = address.sdt_reg_xm << XM_START_BIT;
+    sf = address.sdt_ll_sf; // Boolean so no shift
+    imm12 = address.sdt_imm12 << IMM12_START_BIT;
+    simm9 = address.sdt_simm9 << SIMM9_START_BIT;
+    simm19 = address.ll_simm19 << SIMM19_START_BIT;
+
+    // Constructing binary instruction
+    instruction |= rt; 
+    instruction |= SDT_L; // As load instruction
+    if (is_sdt(mode)) {
+        instruction |= SDT_BASE;
+        instruction |= xn;
+    } else { // Load literal instruction
+        instruction |= LL_BASE;
+        instruction |= simm19;
+    }
+
+    // Handle different addressing modes
+    switch (mode) {
+        case ZERO_OFFSET:
+            // Just want the value in xn and no other offset
+            instruction |= SDT_U;
+            break;
+        case UNSIGNED_OFFSET:
+            instruction |= SDT_U;
+            instruction |= imm12;
+            // Assumes division handled in parse_address function
+            break;  
+        case PRE_INDEXED:
+            instruction |= SDT_I;
+            instruction |= SDT_INDEXED;
+            instruction |= simm9;
+            break;
+        case POST_INDEXED:
+            instruction |= SDT_INDEXED;
+            instruction |= simm9;
+            break;
+        case REGISTER_OFFSET:
+            instruction |= SDT_REG;
+            instruction |= xm;
+            break;
+        case LOAD_LITERAL:
+            // Need to break out but behaviour all already handled
+            break;
+        default:
+            // Invalid instruction
+            exit(1);
+    }
+
+    if (sf) { // 64-bit mode
+        instruction |= SDT_LL_SIGN_FLAG;
+    }
+
+    return instruction;
+}
--- src/assemble_data_transfer.h
+++ src/assemble_data_transfer.h
@@ -6,8 +6,8 @@
 #include "symbol_table.h"
 #include "tokenizer.h"
 
-// Adding the below in the correct combination, along with the other relevant
-// components will give the final binary instruction
+// These are all fixed bits that can't be extracted from the assembly
+// and are needed to be included in specific instructions
 #define SDT_BASE (0x2E0UL << 22) 
 // 1 0 1 1 1 0 0 0 0 0 
 // Bits 22 - 31 (not including bits 22, 24, and 30 as set elsewhere) 
@@ -15,8 +15,13 @@
 // 0 0 0 1 1 0 0 0
 // Bits 24 - 31 (not including bit 30 as set elsewhere)
 #define SDT_LL_SIGN_FLAG (1 << 30)// Bit 30
-#define SDT_U (1 << 24)// Bit 24
-#define SDT_L (1 << 22)// Bit 22
+#define SDT_U (1UL << 24)// Bit 24
+#define SDT_L (1UL << 22)// Bit 22
+#define SDT_I (1UL << 11)// Bit 11
+#define SDT_INDEXED (1UL << 10)// Bit 10 - denotes an pre-indexed or post-indexed instruction
+#define SDT_REG (0x81AUL << 10)
+// 1 0 0 0 0 0 0 1 1 0 1 0
+// Bits 10 - 21 (not including bits 16 - 20)
 
 typedef enum {
     ZERO_OFFSET,
@@ -32,9 +37,10 @@ typedef struct {
     uint32_t sdt_ll_reg_rt;
     uint32_t sdt_reg_xn;
     uint32_t sdt_reg_xm;
-    int32_t sdt_imm12; 
-    uint32_t sdt_simm9;
-    uint32_t ll_simm19;
+    bool sdt_ll_sf;
+    uint32_t sdt_imm12; // Handle sf-based division in parse_address
+    int32_t sdt_simm9;
+    int32_t ll_simm19;
 } ParsedAddress;
 
 // Parse address to populate ParsedAddress struct
